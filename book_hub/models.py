b303598c67d5c57382e2142df77ac1b3d7460a08
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-07-25 08:18:08 +0000

    models & migrations

--- book_hub/models.py
+++ book_hub/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField()
+    rating = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.title} (Rating: {self.rating})"
