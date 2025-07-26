0e041cf00d00f40a79a5ff5804986f2fed58ac9f
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-07-26 06:19:08 +0000

    Data models - default & validators

--- book_hub/models.py
+++ book_hub/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField()
-    rating = models.IntegerField()
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
 
 
     def __str__(self):
-        return f"{self.title} (Rating: {self.rating})"
+        return f"title='{self.title}', author='{self.author}', rating={self.rating}, is_bestselling={self.is_bestselling}"
+
