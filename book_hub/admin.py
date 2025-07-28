531c820aebfda7e4d1738fa09b746a2f1a1a1d69
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-07-28 12:24:30 +0000

    ModelAdmin & config options

--- book_hub/admin.py
+++ book_hub/admin.py
@@ -4,4 +4,10 @@ from django.contrib import admin
 from .models import Book
 
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+    # prepopulated_fields = {"slug": ("title",)}     #No need, since using custom auto populated unique slug generation
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
+
+admin.site.register(Book, BookAdmin)
