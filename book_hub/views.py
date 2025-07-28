8cae39d76f8800f3c22b2d3ae914a895541ec047
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-07-28 10:32:40 +0000

    Aggregation & Ordering

--- book_hub/views.py
+++ book_hub/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
 
 def book_hub_index(request):
-    books = Book.objects.all()
-    return render(request, "book_hub/index.html", {"books": books})
+    books = Book.objects.all().order_by("title") #.order_by("-rating") - For decending order
+    total_books = books.count()
+    avg_rating = round(books.aggregate(Avg("rating"))["rating__avg"], 2)
+    return render(request, "book_hub/index.html", {
+        "books": books,
+        "total_books": total_books,
+        "avg_rating": avg_rating,})
 
 
 def book_detail(request, slug):
@@ -15,4 +21,9 @@ def book_detail(request, slug):
     # except:
     #     raise Http404()
     book = get_object_or_404(Book, slug=slug)
-    return render(request, "book_hub/book_detail.html", {"book": book})
+    return render(request, "book_hub/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling,
+        })
